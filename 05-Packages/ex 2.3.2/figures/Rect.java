85d990db1d1dd59d02020696c1fd747fbe2cc0fe
Gabriel Felipe <73765487+gabrielfpcarvalho@users.noreply.github.com>
2022-02-27 14:03:40 -0300

    Create Rect.java

---
+++ 05-Packages/ex 2.3.2/figures/Rect.java
@@ -0,0 +1,31 @@
+package figures;
+
+import java.awt.*;
+
+public class Rect 
+{
+    private int x, y;
+    private int w, h;
+
+    public Rect (int x, int y, int w, int h)
+     {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    public void print () 
+    {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g, Color fillColor,Color drawColor) 
+    {
+        g.setColor(drawColor);
+        g.drawRect(this.x, this.y, this.w,this.h);
+        g.setColor(fillColor);
+        g.fillRect(this.x, this.y, this.w, this.h);
+    }    
+}
