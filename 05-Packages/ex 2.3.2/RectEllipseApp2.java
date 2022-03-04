fd7af4f6599040b7d84a1739fb2b00bd881a1845
Gabriel Felipe <73765487+gabrielfpcarvalho@users.noreply.github.com>
2022-03-04 11:51:10 -0300

    Update RectEllipseApp2.java

--- 05-Packages/ex 2.3.2/RectEllipseApp2.java
+++ 05-Packages/ex 2.3.2/RectEllipseApp2.java
@@ -17,6 +17,7 @@ class RectEllipseFrame extends JFrame
 {
     Rect r1,r2,r3;
     Ellipse e1,e2,e3;
+    Line l1, l2, l3;
 
     RectEllipseFrame () 
     {
@@ -29,7 +30,7 @@ class RectEllipseFrame extends JFrame
                 }
             }
         );
-        this.setTitle("Rect + Ellipse");
+        this.setTitle("Rect + Ellipse + Line");
         this.setSize(350, 350);
 
         this.r1 = new Rect(50,50, 100,30);
@@ -39,6 +40,11 @@ class RectEllipseFrame extends JFrame
         this.e1 = new Ellipse(70,200, 100,50);
         this.e2 = new Ellipse(210,40, 120,30);
         this.e3 = new Ellipse(200,100, 70,220);
+
+        this.l1 = new Line(130,180, 160,180);
+        this.l2 = new Line(160,180, 145,90);
+        this.l3 = new Line(145,90, 130,180);
+
 
     }
 
@@ -53,5 +59,9 @@ class RectEllipseFrame extends JFrame
         this.e1.paint(g,Color.yellow, Color.cyan);
         this.e2.paint(g,Color.white, Color.pink);
         this.e3.paint(g,Color.black, Color.red);
+
+        this.l1.paint(g,Color.blue);
+        this.l2.paint(g,Color.yellow);
+        this.l3.paint(g,Color.red);
     }
 }
