7bbbf499b752dd4f29fd84d81f2af3e1cc27fa3b
Gabriel Felipe <73765487+gabrielfpcarvalho@users.noreply.github.com>
2022-03-13 18:54:23 -0300

    Create ListApp.java

---
+++ 07-Figure/ex 2.6.3/ListApp.java
@@ -0,0 +1,73 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+import figures.*;
+
+class ListApp {
+    public static void main (String[] args) {
+        ListFrame frame = new ListFrame();
+        frame.setVisible(true);
+    }
+}
+
+class ListFrame extends JFrame {
+    ArrayList<Figure> figlist = new ArrayList<Figure>();
+    Random rand = new Random();
+    
+
+    ListFrame () {
+        this.addWindowListener (
+            new WindowAdapter() {
+                public void windowClosing (WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+
+        this.addKeyListener (
+            new KeyAdapter() {
+                public void keyPressed (KeyEvent evt) {
+                    if (evt.getKeyChar() == 'r') {
+                        int x = rand.nextInt(350);
+                        int y = rand.nextInt(350);
+                        int w = rand.nextInt(50);
+                        int h = rand.nextInt(50);
+                        figlist.add(new Rect(x,y, w,h));
+                        repaint();  // outer.repaint()
+                    }
+                    else if (evt.getKeyChar() == 'e') {
+                        int x = rand.nextInt(350);
+                        int y = rand.nextInt(350);
+                        int w = rand.nextInt(50);
+                        int h = rand.nextInt(50);
+                        figlist.add(new Ellipse(x,y, w,h));
+                        repaint();  // outer.repaint()
+                    }
+                    else if (evt.getKeyChar() == 'l') {
+                        int x = rand.nextInt(350);
+                        int y = rand.nextInt(350);
+                        int x2 = rand.nextInt(350);
+                        int y2 = rand.nextInt(350);
+                        figlist.add(new Line(x,y, x2,y2));
+                        repaint();
+                    }
+                }
+            }
+        );
+
+        this.setTitle("Lista Heterogenea de Retengulos, Elipses e Linhas");
+        this.setSize(350, 350);
+    }
+
+    public void paint (Graphics g)
+    {
+        super.paint(g);
+        for (Figure fig: this.figlist)
+        {
+            fig.paint(g);
+        }
+    }
+}
