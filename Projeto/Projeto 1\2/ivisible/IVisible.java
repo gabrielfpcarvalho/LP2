438e1f3d05d43416e7064c8c430f2e4299545b93
Gabriel Felipe <73765487+gabrielfpcarvalho@users.noreply.github.com>
2022-04-24 20:48:25 -0300

    Update IVisible.java

--- Projeto/Projeto 1\2/ivisible/IVisible.java
+++ Projeto/Projeto 1\2/ivisible/IVisible.java
@@ -4,7 +4,7 @@ import java.awt.Graphics;
 import java.awt.Point;
 
 public interface IVisible {
-    public void    paint (Graphics g);
-    public boolean area (Point point);  //método area serve para verificar se o mouse clicou dentro da área da figura
+    void    paint (Graphics g);
+    boolean area (Point point);  //método area serve para verificar se o mouse clicou dentro da área da figura
     //método area foi criado pois a linha é diferente de calcular do retangulo e elipse e dos botões
 }
